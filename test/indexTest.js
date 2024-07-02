7924f591b6c4dd386b817a3be3366da456bda14d
Mark <mark.njau@student.moringaschool.com>
2024-07-02 19:54:41 +0300

    first commit

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ require ( './helpers.js' );
 
 const sinon = require( 'sinon' )
 
-describe('index.js', function () {
+ describe('index.js', function () {
   describe('findMatching()', function () {
     it('returns all drivers that match the passed in name', function () {
       const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
